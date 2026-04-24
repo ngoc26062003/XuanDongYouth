0fba056e94cfc899e3e6b0006b80b1617d940b76
Van Thi Muoi Ngoc <vanthimuoingoc@gmail.com>
2026-04-24 16:21:23 +0700

    update_env

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  build: {
+    // Adjusts the chunk size warning limit (in kB). Default is 500.
+    chunkSizeWarningLimit: 1000,
   },
-  define: {
-    'process.env': {}
-  }
 });
